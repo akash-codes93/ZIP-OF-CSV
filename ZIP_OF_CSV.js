cd8ca34d789e6649ba44fa71de12fdb709174c89
akash <akash5gupta5@gmail.com>
2017-02-09 17:05:08 +0530

    initial

---
+++ ZIP_OF_CSV.js
@@ -0,0 +1,53 @@
+	var zip = new JSZip();
+	
+	var csv = function () {
+		
+		var csv = JSONToCSVConvertor(JSON_DATA,FIELD_NAME, "", true);
+
+		return csv;	
+		
+	}();
+	
+	zip.file(FILE_NAME, csv);
+	zip.generateAsync({
+		type: "blob"
+	}).then(function(blob) {
+		saveAs(blob, "ZIP_FILE.zip");
+	});
+	
+	function JSONToCSVConvertor(JSONData,data_names, ReportTitle, ShowLabel) {
+
+        var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+
+        var CSV = '';
+
+        if (true) {
+            var row = "";
+
+            row = data_names;
+
+            row = row.slice(1, -1);
+            row = row.replace(/ /g,'');
+
+            CSV += row + '\r\n';
+        }
+
+        for (var i = 0; i < arrData.length; i++) {
+            var row = "";
+
+            for (var index in arrData[i]) {
+                row += '"' + arrData[i][index] + '",';
+            }
+            row = row.replace(/null/g,' ');
+            row.slice(0, row.length - 1);
+
+            CSV += row + '\r\n';
+        }
+
+        if (CSV == '') {
+            alert("Invalid data");
+            return;
+        }
+
+        return CSV;
+    }
